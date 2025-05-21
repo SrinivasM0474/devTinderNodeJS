861c18564fdfdddaa5381b8cfe366bf2151a53e0
Srinivas Manchikatla <srinivas.manchikatla4@gmail.com>
2025-05-20 22:29:34 -0700

    playing with routes

--- src/app.js
+++ src/app.js
@@ -3,22 +3,9 @@ const express = require("express");
 const app = express();
 
 //This will only handle GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "srinivas", lastName: "M" });
-});
-
-app.post("/user", (req, res) => {
-  console.log("Save data to the database");
-  res.send("Data successfully saved to DB");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
-
-//this will match all the http method api calls to test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
 });
 
 app.listen(4444, () => {

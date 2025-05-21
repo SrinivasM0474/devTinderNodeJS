e22cdb630f51a672a136d425593b9dd9b8c704d5
Srinivas Manchikatla <srinivas.manchikatla4@gmail.com>
2025-05-20 22:08:05 -0700

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express = require("express");
 
 const app = express();
 
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "srinivas", lastName: "M" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save data to the database");
+  res.send("Data successfully saved to DB");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
+//this will match all the http method api calls to test
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello welcome node ");
 });
 
 app.listen(4444, () => {

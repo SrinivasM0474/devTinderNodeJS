76304bf82377cecdf25f24d009c3098a8a563ecc
Srinivas Manchikatla <srinivas.manchikatla4@gmail.com>
2025-05-21 00:07:46 -0700

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,38 +2,22 @@ const express = require("express");
 
 const app = express();
 
-//All are similar wraaping inside an array
-// app.use("/route", rh1, rh2, rh3, rh4, rh5);
-// app.use("/route", [rh1, rh2, rh3, rh4, rh5]);
-// app.use("/route", [rh1, rh2], rh3, rh4, rh5);
-// app.use("/route", rh1, rh2, [rh3, rh4], rh5);
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.get("/user", [
-  (req, res, next) => {
-    //route handler
-    console.log("Route 1");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route 2");
-    // res.send("Response 2");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route 3");
-    // res.send("Response 3");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route 4");
-    // res.send("Response 4");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route 5");
-    res.send("Response 5");
-  },
-]);
+//Handle Auth Middleware for all requests GET,POST,PATCH, PUT, DELETE... requests
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("User data sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
 
 app.listen(4444, () => {
   console.log("Server is successfully listening on port 4444");

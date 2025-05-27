4942d90c2d23260d89f686da46a1eae819d788c0
Srinivas Manchikatla <srinivas.manchikatla4@gmail.com>
2025-05-27 15:42:51 -0700

    razorpay changes

--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 
 var instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID, //comes from Razorpay dashboard
-  key_secret: process.env.RAZORPAY_KEY_SECRET, //comes from Razorpay dashboard
+  key_id: "process.env.RAZORPAY_KEY_ID", //comes from Razorpay dashboard
+  key_secret: "process.env.RAZORPAY_KEY_SECRET", //comes from Razorpay dashboard
 });
 
 module.exports = instance;

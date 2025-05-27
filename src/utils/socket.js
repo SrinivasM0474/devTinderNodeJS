7b5cd8fe93c18895fbc45d1bf0bb3db7533925cc
Srinivas Manchikatla <srinivas.manchikatla4@gmail.com>
2025-05-27 13:55:02 -0700

    Integrated Live chat saved in DB

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,9 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const Chat = require("../models/chat");
+const ConnectionRequest = require("../models/connectionRequest");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -14,25 +18,79 @@ const initializeSocket = (server) => {
     },
   });
 
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
+    //check token is valid or not
+    const token = socket.handshake.auth.token;
+    const decodedObj = jwt.verify(token, "DEV@Tinder$474");
+    const { _id } = decodedObj;
+    const user = await User.findById(_id);
+    // console.log("TOKEN", token);
+    // console.log("USER", user);
+    if (!user) {
+      return;
+    }
+
     socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
       const roomId = getSecretRoomId(userId, targetUserId);
-      //   console.log(firstName + " Joined room:", roomId);
+      // console.log(firstName + " Joined room:", roomId);
       socket.join(roomId);
     });
 
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      //   console.log(
-      //     `Message from ${firstName} (${userId}) to ${targetUserId}: ${text}`,
-      //   );
-      io.to(roomId).emit("messageReceived", {
-        firstName,
-        userId,
-        targetUserId,
-        text,
-      });
-    });
+    socket.on(
+      "sendMessage",
+      async ({ firstName, lastName, userId, targetUserId, text }) => {
+        //   console.log(
+        //     `Message from ${firstName} (${userId}) to ${targetUserId}: ${text}`,
+        //   );
+        //Save message to database here if needed
+        try {
+          const roomId = getSecretRoomId(userId, targetUserId);
+
+          //check if userId & targetUserId are friends
+          const isFriends = await ConnectionRequest.findOne({
+            $or: [
+              {
+                fromUserId: userId,
+                toUserId: targetUserId,
+                status: "accepted",
+              },
+              {
+                fromUserId: targetUserId,
+                toUserId: userId,
+                status: "accepted",
+              },
+            ],
+          });
+          // console.log("isFriends", isFriends);
+
+          if (!isFriends) {
+            return;
+          }
+
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+          await chat.save();
+          io.to(roomId).emit("messageReceived", {
+            firstName,
+            lastName,
+            text,
+          });
+        } catch (error) {
+          console.error("Error saving message to database:", error);
+        }
+      },
+    );
 
     socket.on("disconnect", () => {});
   });
